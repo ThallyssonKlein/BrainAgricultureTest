c72c4c8166ba0308a7eba534027f221ad4b3c76b
thallyssonklein <thallyssonklein@gmail.com>
2025-01-12 15:29:28 -0300

    feat: readme + mocked data

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 ```
 docker compose -f docker-compose-dev.yml up -d --build
 ```
+
+## How to access
+
+### The frontend
+
+```
+http://localhost:3000
+```
+
+### The API Docs
+
+```
+http://localhost:8080/docs
+```
--- init.dev.sql
+++ init.dev.sql
@@ -29,3 +29,27 @@ CREATE TABLE crops (
     culture_id INTEGER NOT NULL REFERENCES cultures(id),
     farm_id INTEGER NOT NULL REFERENCES farms(id)
 );
+
+INSERT INTO farmers (document, name, city, state)
+VALUES 
+('12345678901234', 'João da Silva', 'São Paulo', 'SP');
+
+INSERT INTO farms (name, arable_area, vegetation_area, total_area, farmer_id, city, state)
+VALUES
+('Fazenda Sol Nascente', 100.5, 50.2, 150.7, 1, 'Campinas', 'SP'),
+('Fazenda Estrela Guia', 200.0, 100.0, 300.0, 1, 'Sorocaba', 'SP'),
+('Fazenda Boa Esperança', 50.0, 25.0, 75.0, 1, 'Piracicaba', 'SP');
+
+INSERT INTO cultures (name, farmer_id)
+VALUES
+('Milho', 1),
+('Soja', 1),
+('Cana-de-Açúcar', 1);
+
+INSERT INTO crops (date, culture_id, farm_id)
+VALUES
+('2025-01-15', 1, 1),
+('2025-01-16', 2, 2),
+('2025-01-17', 3, 3),
+('2025-02-01', 1, 2),
+('2025-02-10', 2, 3);
